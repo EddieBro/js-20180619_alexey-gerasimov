1b2676bbc18b21197b9be0f5fe217a1ac077d8af
Alexey Gerasimov <eddiebrock13@gmail.com>
2018-07-01 23:06:37 +0500

    3.2 решено, наконец то

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,6 +4,34 @@ let calendar = {
 };
 
 calendar[Symbol.iterator] = function () {
+    let current = new Date(this.from).getTime();
+    let last = new Date(this.to).getTime();
+    const day = 1000 * 60 * 60 * 24;
+    let dateArr = [];
+    let counter = 0;
+    return {
+        next() {
+            if (current <= last) {
+                current = current + day;
+                counter++;
+                if(new Date(current).getDay() === 6 || new Date(current).getDay() === 0)
+                {
+                    dateArr.push(`[${("0" + new Date(current).getDate()).slice(-2)}]`);
+                } else {
+                    dateArr.push(`${("0" + new Date(current).getDate()).slice(-2)}`);
+                }
+                return {
+                    done: false,
+                    value: dateArr[counter - 1]
+                };
+            } else {
+                return {
+                    value: dateArr,
+                    done: true
+                };
+            }
+        }
+    }
 };
 
 
